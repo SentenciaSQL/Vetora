f09973eb77ee1bf6e07a6510167b63e12e5ad59c
Andrés Frías <inf512.andres.frias@gmail.com>
2026-09-21 17:53:43 -0400

    cambiao applicationId

--- mobile/android/app/src/main/kotlin/com/animalin/animalin/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.animalin.animalin
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ mobile/android/app/src/main/kotlin/com/sentenciasql/lunaveta/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.sentenciasql.lunaveta
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
